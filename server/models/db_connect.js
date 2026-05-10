c830ff474cd5847dc419dec08b6ddea74cf4ef59
macklev <mackenzielevitt03@gmail.com>
2026-05-10 15:11:02 -0400

    Connecting project to be used by others online

--- server/models/db_connect.js
+++ server/models/db_connect.js
@@ -1,17 +1,18 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const con= mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+const con = mysql.createConnection({
+    host: process.env.MYSQL_HOST || process.env.MYSQLHOST,
+    user: process.env.MYSQL_USER || process.env.MYSQLUSER,
+    password: process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DB || process.env.MYSQLDATABASE,
+    port: process.env.MYSQL_PORT || process.env.MYSQLPORT || 3306
 });
 
-const query= (sql, binding) => {
+const query = (sql, binding) => {
     return new Promise((resolve, reject) => {
         con.query(sql, binding, (err, result) => {
-            if(err) reject(err);
+            if (err) reject(err);
             resolve(result);
         });
     });
