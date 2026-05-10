a3170508cf4637b11b4c7ed4a97064c64c405a8d
macklev <mackenzielevitt03@gmail.com>
2026-05-10 14:07:11 -0400

    fixed email

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -8,6 +8,7 @@ const profileMessage = document.getElementById("profile-message");
 const profilePreview = document.getElementById("profile-preview");
 const profileName = document.getElementById("profile-name");
 const profileHandle = document.getElementById("profile-handle");
+const profileEmail = document.getElementById("profile-email");
 const profileBioDisplay = document.getElementById("profile-bio-display");
 
 if (!user) {
