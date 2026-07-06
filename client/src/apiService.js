8dbc125907ac81292be77fbcb38df40427287e4a
macklev <mackenzielevitt03@gmail.com>
2026-07-06 16:25:57 -0400

    safety catches

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,5 +1,9 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
+if (!API_BASE_URL) {
+  throw new Error("REACT_APP_API_BASE_URL is missing from .env");
+}
+
 export async function fetchAPI(route, data = {}, method = 'GET') {
   const options = {
     method,
